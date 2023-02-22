82455615ca357b5775c7fd74ce87728b81bef7ad
Captain Ravioli <{ID}+{username}@users.noreply.github.com>
2023-02-22 21:16:49 +0300

    fix

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function refreshTime() {
     const timeDisplay = document.getElementById("time");
     const date = new Date();
-    var dateString = parseInt(date.getHours()) % 12 + ":" + date.getMinutes() + ":" + date.getSeconds();
+    var dateString = parseInt(date.getHours()) % 12;
+    dateString += ":";
+    if (date.getMinutes() < 10)
+        dateString += "0" + date.getMinutes();
+    else
+        dateString += date.getMinutes();
+    dateString += ":";
+    if (date.getSeconds() < 10)
+        dateString += "0" + date.getSeconds();
+    else
+        dateString += date.getSeconds();
     if (date.getHours() > 12)
         dateString += " PM";
     else
         dateString += " AM";
     timeDisplay.textContent = dateString;
-  }
-    setInterval(refreshTime, 1000);
+}
+
+setInterval(refreshTime, 1);
