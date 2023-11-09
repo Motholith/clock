41e7af48624dc59f834bf5c2a7caa21dccd7e6ea
Captain Ravioli <48518572+Captain-Ravioli@users.noreply.github.com>
2023-11-09 19:38:37 +0300

    Aesthetic improvements, added date

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// id=time
 function refreshTime() {
-    const timeDisplay = document.getElementById("time");
+	const timeDisplay = document.getElementById("time");
     const date = new Date();
-    var dateString = parseInt(date.getHours()) % 12;
-    if (dateString == "0")
-        dateString = "12"
+    var dateString = parseInt(date.getHours()) // % 12;
+    // if (dateString == "0")
+    //     dateString = "12"
     dateString += ":";
     if (date.getMinutes() < 10)
         dateString += "0" + date.getMinutes();
@@ -14,11 +15,46 @@ function refreshTime() {
         dateString += "0" + date.getSeconds();
     else
         dateString += date.getSeconds();
-    if (date.getHours() >= 12)
-        dateString += " PM";
-    else
-        dateString += " AM";
+    // if (date.getHours() >= 12)
+    //     dateString += " PM";
+    // else
+    //     dateString += " AM";
     timeDisplay.textContent = dateString;
 }
-
 setInterval(refreshTime, 1);
+
+// id=date
+function refreshDate() {
+	const dateDisplay = document.getElementById("date");
+    const date = new Date();
+	var dayNum = parseInt(date.getDay());
+	switch (dayNum) {
+		case 0:
+			dayNum = "Sunday";
+			break;
+		case 1:
+			dayNum = "Monday";
+			break;
+		case 2:
+			dayNum = "Tuesday";
+			break;
+		case 3:
+			dayNum = "Wednesday";
+			break;
+		case 4:
+			dayNum = "Thursday";
+			break;
+		case 5:
+			dayNum = "Friday";
+			break;
+		case 6:
+			dayNum = "Saturday";
+			break;
+	}
+	var dateString = dayNum + " ";
+	dateString += date.getDate() + "/";
+	dateString += date.getMonth() + "/";
+	dateString += date.getFullYear();
+	dateDisplay.textContent = dateString;
+}
+setInterval(refreshDate, 1);
